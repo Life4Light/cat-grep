ea5a4cabbae48c4cdeb5f31740e5e4f0c7d3b585
manwoodp <seeds-ata@mail.ru>
2023-10-21 15:47:34 +0300

    added -v flag, fixed multiply files

--- src/cat/flags-cat.h
+++ src/cat/flags-cat.h
@@ -3,20 +3,37 @@
 #ifndef C3_SIMPLEBASHUTILS_1_FLAGS_CAT_H
 #define C3_SIMPLEBASHUTILS_1_FLAGS_CAT_H
 #include "stdbool.h"
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
 
 typedef struct flags_cat{
-    bool b;
-    bool e;
-    bool n;
-    bool s;
-    bool t;
+	bool b;
+	bool e;
+	bool n;
+	bool s;
+	bool t;
 }flags_cat;
-void init_flags(struct flags_cat *flags){
-    (*flags).b = false;
-    (*flags).e = false;
-    (*flags).n = false;
-    (*flags).s = false;
-    (*flags).t = false;
-};
+void init_flags(struct flags_cat *flags);
+
+void e_option(const char *str);
+
+void t_option(char *string);
+
+void n_option(const char *buffer, int buffer_index, int *number);
+
+void b_option(const char *buffer, int i, int *pInt);
+
+bool s_option(char *buffer, bool *is_last_empty);
+
+void v_option(const char ch);
+
+
+
+
 #endif
 
--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -1,117 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "flags-cat.h"
-#include <stdbool.h>
-#include <unistd.h>
-#include <string.h>
-void e_option(const char *str);
 
-void t_option(char *string);
 
-void n_option(const char *buffer, int buffer_index, int *number);
 
-void b_option(const char *buffer, int i, int *pInt);
+int main(int argc, char *argv[]) {
+	struct flags_cat flags;
+	char **filenames;
+	init_flags(&flags);
+	char buffer[256];
+	int rez;
+	int files_count;
+	bool is_double_empty_str = false;
+	bool is_double_empty = false;
+	while ((rez = getopt(argc, argv, "benst")) != -1) {
+		switch (rez) {
+			case 'b':
+				flags.b = true;
+				break;
+			case 'e':
+				flags.e = true;
+				break;
+			case 'n':
+				flags.n = true;
+				break;
+			case 's':
+				flags.s = true;
+				break;
+			case 't':
+				flags.t = true;
+				break;
+			default:
+				break;
+		}
+	}
+	files_count = argc - optind;
+	filenames = (char **) malloc(files_count * sizeof(int *));
+	for (int i = 0; i < files_count; i++){
+		filenames[i] = (int *) malloc(255 * sizeof(int));
+		int counter_n_option = 1;
+		int counter_b_option = 1;
+		strcpy(filenames[i], argv[optind + i]);
+		FILE *fp = fopen(filenames[i], "r");
+		if(!fp){
+			printf("file not correct");
+			return 1;
+		}
+		while((fgets(buffer, 256, fp)) != NULL)
+		{
+			for(int i = 0; i < strlen(buffer); i++) {
+				if(flags.s == true){
+					is_double_empty = s_option(buffer, &is_double_empty_str);
+				}
+				if(flags.n == true && flags.b == false){
+					n_option(buffer, i, &counter_n_option);
+				}
+				if(flags.b == true){
+					b_option(buffer, i, &counter_b_option);
+				}
+				if(flags.e == true) {
+					v_option(buffer[i]);
+					e_option(&buffer[i]);
+				}
+				if(flags.t == true) {
+					v_option(buffer[i]);
+					t_option(&buffer[i]);
+				}
+				if(is_double_empty == false && (!iscntrl(buffer[i]) || buffer[i] == '\n')){
+					printf("%c", buffer[i]);
+				}
+			}
+		}
+		fclose(fp);
+	}
+	for (int i = 0; i < files_count; i++)
+		free(filenames[i]);
 
-bool s_option(char *buffer, bool *is_last_empty);
+	free(filenames);
 
-int main(int argc, char *argv[]){
-    struct flags_cat flags;
-    char filename[255];
-    init_flags(&flags);
-    char buffer[256];
-    int rez = 0;
-    int counter_n_option = 1;
-    int counter_b_option = 1;
-    bool is_double_empty_str = false;
-    bool is_double_empty = false;
-    while ((rez = getopt(argc, argv, "benst")) != -1){
-        switch (rez) {
-            case 'b': flags.b = true; break;
-            case 'e': flags.e = true; break;
-            case 'n': flags.n = true; break;
-            case 's': flags.s = true; break;
-            case 't': flags.t = true; break;
-            default: break;
-        }
-    }
-    for (int i = 0; i < argc; i++)
-    {
-        if(argv[i][0] != '-'){
-            strcpy(filename, argv[i]);
-        }
-    }
-    printf("%s\n", filename);
-    FILE *fp = fopen(filename, "r");
-    if(!fp){
-        printf("file not correct");
-        return 1;
-    }
-    while((fgets(buffer, 256, fp)) != NULL)
-    {
-        size_t string_length = strlen(buffer);
-        for(int i = 0; i < strlen(buffer); i++) {
-            if(flags.s == true){
-                 is_double_empty = s_option(buffer, &is_double_empty_str);
-            }
-            if(flags.n == true && flags.b == false){
-                n_option(buffer, i, &counter_n_option);
-            }
-            if(flags.b == true){
-                b_option(buffer, i, &counter_b_option);
-            }
-            if(flags.e == true) {
-                e_option(&buffer[i]);
-            }
-            if(flags.t == true) { // does not work
-                t_option(&buffer[i]);
-            }
-            if(is_double_empty == false){
-                printf("%c", buffer[i]);
-            }
-        }
-    }
-    fclose(fp);
 }
 
-bool s_option(char *buffer, bool *is_last_empty) {
-    bool res = false;
-    if (*is_last_empty == true && strlen(buffer) == 1 && buffer[0] == '\n'){
-        buffer[0] = '\0';
-        res = true;
-    } else if(strlen(buffer) == 1 && buffer[0] == '\n'){
-        *is_last_empty = true;
-        res = false;
-    } else{
-        *is_last_empty = false;
-    }
-    return res;
-}
 
-void b_option(const char *buffer, int i, int *pInt) {
-    if(i == 0 && buffer[i] != '\n' && buffer[0] != '\0'){
-        printf("%d\t", *pInt);
-        *pInt = *pInt + 1;
-    } else if(i == 0 && buffer[0] != '\0'){
-        printf("\t");
-    }
-}
 
-void n_option(const char *buffer, int buffer_index, int *number) {
-    if(buffer_index == 0 && buffer[0] != '\0'){
-        printf("%d\t", *number);
-        *number = *number + 1;
-    }
-}
-
-void t_option(char *string) {
-    if(string[0] == '\t'){
-        printf("^");
-        string[0] = 'I';
-    }
-}
-
-void e_option(const char *str){
-    if(str[0] == '\n'){
-        printf("$");
-    }
-}
